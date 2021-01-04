d186300c643b641b907a375ec3ccee49a1f31b19
shiraalter <shiraalter@gmail.com>
2021-01-04 00:06:54 -0500

    functioning app so far

--- app/src/main/java/com/example/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/triviaapp/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.triviaapp;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -16,12 +14,27 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
-import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
+import java.util.Arrays;
+
 import static lib.DialogUtils.showInfoDialog;
 
 public class MainActivity extends AppCompatActivity {
     private Snackbar mSnackBar;
+
+
+    private TextView questionTextView, feedbackTextView;
+    private Button option1Button, option2Button, option3Button, option4Button;
+    private Question currentQuestion;
+    private QuestionBank questionBank;
+    private int score;
+
+
+
+
+
     // Preference booleans; indicates if these respective settings currently enabled/disabled
     private boolean mPrefUseAutoSave;
 
@@ -34,14 +47,112 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupFields();
         setupToolbar();
         setupFAB();
-        restoreAppSettingsFromPrefs();
+        setButtonListeners();
+        //restoreAppSettingsFromPrefs();
 
-        mSnackBar =
-                Snackbar.make(findViewById(android.R.id.content), "Welcome!",
-                        Snackbar.LENGTH_LONG);
+
     }
+
+    private void setupFields() {
+        questionTextView = findViewById(R.id.question);
+        feedbackTextView = findViewById(R.id.feedbackMessage);
+        option1Button = findViewById(R.id.option1);
+        option2Button = findViewById(R.id.option2);
+        option3Button = findViewById(R.id.option3);
+        option4Button = findViewById(R.id.option4);
+
+        questionBank = createQuestionBank();
+
+        currentQuestion = questionBank.getNextQuestion();
+        this.showQuestion(currentQuestion);
+
+    }
+
+    private void setButtonListeners() {
+        option1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int indexOfOptionSelected = 0;
+
+                validateChoice(indexOfOptionSelected);
+            }
+        });
+
+        option2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int indexOfOptionSelected = 1;
+
+                validateChoice(indexOfOptionSelected);
+            }
+        });
+
+        option3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int indexOfOptionSelected = 2;
+
+               validateChoice(indexOfOptionSelected);
+            }
+        });
+
+        option4Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int indexOfOptionSelected = 3;
+
+                validateChoice(indexOfOptionSelected);
+
+            }
+        });
+
+    }
+
+    private void validateChoice(int indexOfOptionSelected) {
+        if(indexOfOptionSelected == currentQuestion.getAnswerIndex()){
+            //answer is correct!
+            feedbackTextView.setText(R.string.correct_message);
+
+            questionBank.removeQuestion(currentQuestion);
+
+            if(questionBank.getSize() != 0) {
+                currentQuestion = questionBank.getNextQuestion();
+                this.showQuestion(currentQuestion);
+            }
+            else{
+                showInfoDialog(this, "GAME OVER","GREAT JOB! YOU WON! You can restart the game or exit the application");
+                //TODO: disable buttons
+            }
+
+
+        }
+        else{
+            feedbackTextView.setText(R.string.incorrect_answer_message);
+        }
+    }
+
+
+    private void showQuestion(final Question currentQuestion) {
+        questionTextView.setText(currentQuestion.getQuestion());
+        option1Button.setText(currentQuestion.getChoiceList().get(0));
+        option2Button.setText(currentQuestion.getChoiceList().get(1));
+        option3Button.setText(currentQuestion.getChoiceList().get(2));
+        option4Button.setText(currentQuestion.getChoiceList().get(3));
+    }
+
+    private QuestionBank createQuestionBank() {
+        Question question1 = new Question("What color is the sky?", Arrays.asList("Yellow", "Blue", "Pink", "Black"), 1);
+        Question question2 = new Question("Who won the presidental election of 2020?", Arrays.asList("Trump", "Oprah", "Dr. Katz", "Biden"), 3);
+        Question question3 = new Question("What is the first letter", Arrays.asList("A", "B","C", "D"), 0);
+        Question question4 = new Question("What sound does a cow make", Arrays.asList("meow", "woof", "moo", "hello!"), 2);
+
+        return new QuestionBank(Arrays.asList(question1,question2,question3,question4));
+    }
+
+
 
     private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -69,8 +180,120 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStop() {
-        saveToSharedPref();
+       // saveToSharedPref();
         super.onStop();
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.action_about:
+                showAbout();
+                return true;
+            case R.id.action_toggle_auto_save:
+                toggleMenuItem(item);
+                mPrefUseAutoSave = item.isChecked();
+                return true;
+            case R.id.action_statistics:
+                showStatistics();
+                return true;
+            case R.id.new_game:
+                startNewGame();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showStatistics() {
+        Intent intent = new Intent(getApplicationContext(), StatisticsActivity.class);
+        startActivity(intent);
+    }
+
+
+
+        private void startNewGame() {
+           // startGameAndSetBoard(new PMGame(), null, R.string.welcome_new_game);
+            questionBank = createQuestionBank();
+            currentQuestion = questionBank.getNextQuestion();
+            this.showQuestion(currentQuestion);
+
+        }
+
+
+    private void showAbout() {
+        showInfoDialog(this, "Trivia App", "Welcome to our Trivia App!");
+    }
+
+    private void toggleMenuItem(MenuItem item) {
+        item.setChecked(!item.isChecked());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* private void restoreAppSettingsFromPrefs() {
+        // Since this is for reading only, no editor is needed unlike in saveRestoreState
+        SharedPreferences preferences = getSharedPreferences(mKeyPrefsName, MODE_PRIVATE);
+
+        // restore AutoSave preference value
+        mPrefUseAutoSave = preferences.getBoolean(mKeyAutoSave, true);
+    }
+
+    private void saveGameAndBoardToSharedPrefsIfAutoSaveIsOn(SharedPreferences.Editor editor) {
+        System.out.println("placeholder");
+    }
+
+    private void saveSettingsToSharedPrefs(SharedPreferences.Editor editor) {
+        // save "autoSave" preference
+        editor.putBoolean(mKeyAutoSave, mPrefUseAutoSave);
     }
     private void saveToSharedPref() {
         // Create a SP reference to the prefs file on the device whose name matches mKeyPrefsName
@@ -92,73 +315,7 @@ public class MainActivity extends AppCompatActivity {
         // apply the changes to the XML file in the device's storage
         editor.apply();
     }
-
-    private void saveGameAndBoardToSharedPrefsIfAutoSaveIsOn(SharedPreferences.Editor editor) {
-        System.out.println("placeholder");
-    }
-
-    private void saveSettingsToSharedPrefs(SharedPreferences.Editor editor) {
-         // save "autoSave" preference
-        editor.putBoolean(mKeyAutoSave, mPrefUseAutoSave);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        dismissSnackBarIfShown();
-
-        int itemId = item.getItemId();
-        switch (itemId) {
-            case R.id.action_about:
-                showAbout();
-                return true;
-            case R.id.action_toggle_auto_save:
-                toggleMenuItem(item);
-                mPrefUseAutoSave = item.isChecked();
-                return true;
-            case R.id.action_statistics:
-                showStatistics();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void showStatistics() {
-        Intent intent = new Intent(getApplicationContext(), StatisticsActivity.class);
-        startActivity(intent);
-    }
-
-    private void dismissSnackBarIfShown() {
-        if (mSnackBar.isShown())
-            mSnackBar.dismiss();
-    }
-
-    public void startNewGame(@SuppressWarnings("UnusedParameters") MenuItem item) {
-        startNewGame();
-    }
-
-        private void startNewGame() {
-           // startGameAndSetBoard(new PMGame(), null, R.string.welcome_new_game);
-            System.out.println("you clicked new game!");
-        }
-
-
-    private void showAbout() {
-        showInfoDialog(this, "Trivia App", "Welcome to our Trivia App!");
-    }
-
-    private void toggleMenuItem(MenuItem item) {
-        item.setChecked(!item.isChecked());
-    }
-
-    private void restoreAppSettingsFromPrefs() {
-        // Since this is for reading only, no editor is needed unlike in saveRestoreState
-        SharedPreferences preferences = getSharedPreferences(mKeyPrefsName, MODE_PRIVATE);
-
-        // restore AutoSave preference value
-        mPrefUseAutoSave = preferences.getBoolean(mKeyAutoSave, true);
-    }
-
+*/
 
 
 }
---
+++ app/src/main/java/com/example/triviaapp/QuestionBank.java
@@ -0,0 +1,35 @@
+package com.example.triviaapp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class QuestionBank {
+    private List<Question> mQuestionList;
+    private int mNextQuestionIndex;
+
+    public QuestionBank(List<Question> questionList){
+        mQuestionList = new ArrayList<>(questionList);
+
+        //shuffle the list
+        Collections.shuffle(mQuestionList);
+
+        mNextQuestionIndex = 0;
+    }
+
+    public Question getNextQuestion(){
+
+        return mQuestionList.get(mNextQuestionIndex);
+    }
+
+    public void removeQuestion(Question question){
+            mQuestionList.remove(question);
+
+
+    }
+
+    public int getSize(){
+        return mQuestionList.size();
+    }
+
+}
